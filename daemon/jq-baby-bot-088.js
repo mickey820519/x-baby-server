e4144c588af2a1921ea6bf23e0dfbc40bd5b1193
mickey <mickey820519@vip.qq.com>
2024-09-18 11:11:01 +0800

    feat: channel模式

--- daemon/jq-baby-bot-088.js
+++ daemon/jq-baby-bot-088.js
@@ -40,6 +40,14 @@ const jqBabyHttpClient = DobHttpApi.getClient(
   }
 );
 
+// 创建一个Tg机器人实例
+const bot = new TelegramBot(
+  jqBabyBot088ConfigData.telegramBotToken,
+  {
+    polling: true
+  }
+);
+
 async function run() {
   botLogger.info('机器人启动...');
 
@@ -50,130 +58,97 @@ async function run() {
   ctx.font = '24px Song';
   const backgroundImage = await loadImage('./asset/img/photo_msg_bg.png');
 
-  // 创建一个Tg机器人实例
-  const bot = new TelegramBot(
-    jqBabyBot088ConfigData.telegramBotToken,
-    {
-      polling: true
-    }
-  );
-
-  // 获取机器人的信息
-  const botUser = await bot.getMe();
-
-  //已加入群组的id
-  let chatIdSet = new Set();
-  chatIdSet.add(-4577046678);
-
-  // 监听自己加入群组的事件
-  bot.on(
-    'new_chat_members',
-    (ret) => {
-      const chat = ret.chat;
-
-      const newChatMembers = ret.new_chat_members;
-      newChatMembers.forEach(
-        (newChatMember) => {
-          if(newChatMember.id === botUser.id) {
-            chatIdSet.add(chat.id);
-            botLogger.info('加入群组：', chat.id);
-          }
-        }
-      );
-    }
-  );
-
-  // 监听自己离开群组的事件
-  bot.on(
-    'left_chat_member',
-    (ret) => {
-      const chat = ret.chat;
-
-      const leftChatMember = ret.left_chat_member;
-      if(leftChatMember.id === botUser.id) {
-        chatIdSet.delete(chat.id);
-        botLogger.info('离开群组：', chat.id);
-      }
-    }
-  );
-
   // 宝贝推荐
-  let maxRecommendId = 0;
+  let maxRecommendId = null;
 
   while(true) {
-    // 获取进球宝贝推荐
-    botLogger.info('获取进球宝贝推荐...');
-    let res = await jqBabyHttpClient.request(
-      {
-        method: 'GET',
-        url: '/v1/soccer/recommend/live'
-      }
-    );
 
-    if(res.data.code === 0) {
-      let recommendList = res.data.data.recommendList;
+    if(maxRecommendId === null) {
+      maxRecommendId = 0;
+    }
+    else {
+      //等待
+      await DobUtilApi.sleep(
+        {
+          millisecond: 5000
+        }
+      );
+    }
 
-      recommendList.sort(
-        (a, b) => {
-          return b.id - a.id;
+    try {
+      // 获取进球宝贝推荐
+      botLogger.info('获取进球宝贝推荐...');
+      let res = await jqBabyHttpClient.request(
+        {
+          method: 'GET',
+          url: '/v1/soccer/recommend/live'
         }
       );
 
-      botLogger.debug('推荐列表', recommendList);
+      if(res.data.code === 0) {
+        let recommendList = res.data.data.recommendList;
 
-      for(let recommend of recommendList) {
-        if(recommend.id > maxRecommendId) {
-          maxRecommendId = recommend.id;
-          //背景图
-          ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-          //文字
-          let message = `${recommend.match.league.nameCn}\n`;
-          message += `时间：${recommend.minute}\n`;
-          message += `${recommend.match.homeTeam.nameCn}\n`;
-          message += `${recommend.homeTeamG} : ${recommend.guestTeamG}\n`;
-          message += `${recommend.match.guestTeam.nameCn}\n`;
-          message += `推荐 ${recommend.period === 1 ? '半场' : '全场'} 继续进球`;
-          let messageWidth = ctx.measureText(message).width;
-          ctx.fillText(message, (canvas.width - messageWidth) / 2, 50);
-          //生成图片
-          let photoBuffer = canvas.toBuffer();
-          //发送图片
-          for(let chatId of chatIdSet) {
-            await bot.sendPhoto(
-              chatId,
-              photoBuffer,
-              {},
-              {
-                filename: 'awesome',
-                contentType: 'image/png'
-              }
-            );
-
-            message = '';
-            message += '进球宝贝会员地址：http://43\\.154\\.166\\.77:23001/\n';
-            message += '开通会员请找：@jinqiu\\_baobei\n';
-            message += '进球宝贝官方讨论群：https://t\\.me/jinqiubaobei888\n';
-            message += '转U到这个地址自动换trx 10U起换（当前汇率1USDT：6TRX）：`TErTvwnLsMFFFC4jeRXcySFn62EiU8PSTP`\n';
-            await bot.sendMessage(
-              chatId,
-              message,
-              {
-                parse_mode: 'MarkdownV2'
-              }
-            );
+        recommendList.sort(
+          (a, b) => {
+            return b.id - a.id;
           }
-          //清空画布
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
+        );
+
+        botLogger.debug('推荐列表', recommendList);
+
+        for(let recommend of recommendList) {
+          if(recommend.id > maxRecommendId) {
+            maxRecommendId = recommend.id;
+            //清空画布
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            //背景图
+            ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+            //文字
+            let message = `${recommend.match.league.nameCn}\n`;
+            message += `时间：${recommend.minute}\n`;
+            message += `${recommend.match.homeTeam.nameCn}\n`;
+            message += `${recommend.homeTeamG} : ${recommend.guestTeamG}\n`;
+            message += `${recommend.match.guestTeam.nameCn}\n`;
+            message += `推荐 ${recommend.period === 1 ? '半场' : '全场'} 继续进球`;
+            let messageWidth = ctx.measureText(message).width;
+            ctx.fillText(message, (canvas.width - messageWidth) / 2, 50);
+            //生成图片
+            let photoBuffer = canvas.toBuffer();
+            //发送消息
+            let channelUsernameList = jqBabyBot088ConfigData.channelUsernameList;
+            for(let channelUsername of channelUsernameList) {
+              await bot.sendPhoto(
+                channelUsername,
+                photoBuffer,
+                {},
+                {
+                  filename: 'awesome',
+                  contentType: 'image/png'
+                }
+              );
+
+              message = '';
+              message += '进球宝贝会员地址：http://43\\.154\\.166\\.77:23001/\n';
+              message += '开通会员请找：@jinqiu\\_baobei\n';
+              message += '转U到这个地址自动换trx 10U起换（当前汇率1USDT：6TRX）：`TErTvwnLsMFFFC4jeRXcySFn62EiU8PSTP`\n';
+              await bot.sendMessage(
+                channelUsername,
+                message,
+                {
+                  parse_mode: 'MarkdownV2',
+                  link_preview_options: {
+                    is_disabled: true
+                  }
+                }
+              );
+            }
+          }
         }
       }
     }
-
-    //等待30秒
-    await DobUtilApi.sleep(
-      {
-        millisecond: 30000
-      }
-    );
+    catch(err) {
+      botLogger.error(err);
+    }
   }
 }
 
@@ -182,6 +157,7 @@ run().then(
 ).catch(
   (err) => {
     botLogger.error(err);
+    bot.stopPolling();
   }
 ).finally(
   () => {
